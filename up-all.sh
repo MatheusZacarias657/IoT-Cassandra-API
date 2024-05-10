d06d4a9125a2066cad8901901453579f1cae7848
Matheus Zacarias <74462682+MatheusZacarias657@users.noreply.github.com>
2024-05-10 11:39:56 -0300

    improvement bash

--- up-all.sh
+++ up-all.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 echo "Build Applications.."
-docker build -t topicos-api -f Topicos/Dockerfile-API .
-docker build -t topicos-etl -f Topicos/Dockerfile-ETL .
+
+if [ -z "$1" ]; then
+  docker build -t topicos-api -f Topicos/Dockerfile-API .
+  docker build -t topicos-etl -f Topicos/Dockerfile-ETL .
+else
+    docker build -t topicos-etl -f Topicos/Dockerfile-$1 .
+fi
 
 echo ""
 echo "Trying to up Infrastructure..."
-#docker-compose --project-name topicos -f docker-compose-infra.yaml up -d
+docker-compose --project-name topicos -f docker-compose-infra.yaml up -d --no-recreate
 
 echo ""
 echo "Trying to up the System..."
